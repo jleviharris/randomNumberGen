7a9faf11a6f5a1daf6bc4fb87ef7f172eb4a3b27
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-09-19 15:23:11 -0400

    Day 2 of 100 days of javaScript complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const generateNumber = () => {
+    debugger;
+    setRandomNumber(randm);
+  };
+  const randm = Math.floor(Math.random() * 1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Number Generator</h1>
+      <div className="counter">{randomNumber}</div>
+
+      <br />
+
+      <button className="Bttn" onClick={generateNumber}>
+        Generate Number
+      </button>
     </div>
   );
 }
